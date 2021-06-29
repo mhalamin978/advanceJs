98306fcd902cd69d0b95dba2267206610ee4b1fc
alamin <mhalamin978@gamil.com>
2021-06-30 01:21:52 +0600

    jka

---
+++ trueFalse.js
@@ -0,0 +1,16 @@
+const age= '23';
+
+if(age){
+    console.log('condition is true');
+}
+else {
+    console.log("condition is false")
+}
+
+// khali string thakle fal 
+
+//falsy value::
+// false, 0 , "",undefined, null, Nan, 
+
+//truthy
+// "0", ' ', string,  [], {}
