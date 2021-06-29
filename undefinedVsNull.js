eafe5da523addf653902d13ead0c90f1adeed98a
alamin <mhalamin978@gamil.com>
2021-06-30 01:35:07 +0600

    null

---
+++ undefinedVsNull.js
@@ -0,0 +1,23 @@
+//Undefined
+
+let pakhi;
+console.log(pakhi);
+
+function add(num,num2){
+    console.log(num+num2)
+    //return na korle undefined value dibe
+    // return er pore kisu na likhle undefined
+}
+
+const result =  add(34,35);
+console.log(result);
+
+const premik = {name: 'smart', phone: 09898};
+//object nai emn kisu chaile undefined hobe.
+console.log(premik.jaNai);
+
+let fun = undefined;
+console.log(fun);
+
+//you can find undefined in many ways 
+//null means , i have to set there is nothing
